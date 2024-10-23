bd4311124ae4c843134d3e64bc676c38e18de987
zhang_wei <4878+always_end@user.noreply.gitee.com>
2024-10-23 15:29:46 +0800

    对象引用的练习

---
+++ project01/src/head/objectrefrence/Dog.java
@@ -0,0 +1,18 @@
+package head.objectrefrence;
+/**
+ * Dog
+ */
+public class Dog {
+    String name;
+
+    void bark() {
+        System.out.println(name + " bark");
+    }
+    void eat() {
+        System.out.println("eat");
+    }
+    void chaseCat() {
+        System.out.println("chaseCat");
+    }
+    
+}
